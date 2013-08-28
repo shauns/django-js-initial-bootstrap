81d927f2310cc09180cb97e5410bcdb21d60088b
Shaun Stanworth <shaun_stanworth@wordbank.com>
2013-08-28 17:05:30 +0100

    Broken out bootstrapping parent classes

--- django_js_initial_bootstrap/views.py
+++ django_js_initial_bootstrap/views.py
@@ -14,7 +14,7 @@ class BaseBootstrapManager(object):
         @return: A safe string with a Javascript expression to insert directly into a template
         @rtype: SafeString
         """
-        return mark_safe("JSON.parse('%s')" % json_string)
+        return mark_safe("JSON.parse('%s')" % json_string.replace("'", "\\'"))
 
 
 class ListBasicBootstrapMixin(BaseBootstrapManager):
@@ -29,29 +29,7 @@ class ListBasicBootstrapMixin(BaseBootstrapManager):
         return list(qs.values())
 
 
-class ListSerializerBootstrapMixin(ListBasicBootstrapMixin):
-    serializer_class = None
-    model = None
-
-    def prepare_queryset(self, qs):
-        serializer = self.get_serializer(qs)
-        return serializer.data
-
-    def get_serializer_context(self):
-        """
-        Extra context provided to the serializer class.
-        """
-        return {
-            'request': self.request,
-            'format': 'json',
-            'view': self
-        }
-
-    def get_serializer(self, qs):
-        serializer_class = self.get_serializer_class()
-        context = self.get_serializer_context()
-        return serializer_class(qs, many=True, context=context)
-
+class BootstrapSerializerMixin(object):
     def get_serializer_class(self):
         serializer_class = self.serializer_class
         if serializer_class is not None:
@@ -68,14 +46,56 @@ class ListSerializerBootstrapMixin(ListBasicBootstrapMixin):
                 model = self.model
         return DefaultSerializer
 
+    def get_serializer_context(self):
+        """
+        Extra context provided to the serializer class.
+        """
+        return {
+            'request': self.request,
+            'format': 'json',
+            'view': self
+        }
 
-class DetailJsonBootstrapMixin(BaseBootstrapManager):
+
+class ListSerializerBootstrapMixin(BootstrapSerializerMixin, ListBasicBootstrapMixin):
+    serializer_class = None
+    model = None
+
+    def prepare_queryset(self, qs):
+        serializer = self.get_serializer(qs)
+        return serializer.data
+
+    def get_serializer(self, qs):
+        serializer_class = self.get_serializer_class()
+        context = self.get_serializer_context()
+        return serializer_class(qs, many=True, context=context)
+
+
+class DetailBasicBootstrapMixin(BaseBootstrapManager):
     bootstrapped_context_object = 'bootstrapped'
 
     def get_context_data(self, **kwargs):
-        cd = super(DetailJsonBootstrapMixin, self).get_context_data(**kwargs)
-        cd[self.bootstrapped_context_object] = self.json_parse_code(json.dumps(self.prepare_object(self.object)))
+        cd = super(DetailBasicBootstrapMixin, self).get_context_data(**kwargs)
+        object_to_bootstrap = self.get_object_to_bootstrap(self.object)
+        prepared_object = self.prepare_object(object_to_bootstrap)
+        cd[self.bootstrapped_context_object] = self.json_parse_code(json.dumps(prepared_object))
         return cd
+
+    def get_object_to_bootstrap(self, o):
+        return o
 
     def prepare_object(self, o):
         return o
+
+class DetailSerializerBootstrapMixin(BootstrapSerializerMixin, DetailBasicBootstrapMixin):
+    serializer_class = None
+    model = None
+
+    def prepare_object(self, o):
+        serializer = self.get_serializer(o)
+        return serializer.data
+
+    def get_serializer(self, o):
+        serializer_class = self.get_serializer_class()
+        context = self.get_serializer_context()
+        return serializer_class(o, context=context)
